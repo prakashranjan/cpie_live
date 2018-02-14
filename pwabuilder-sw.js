4c455899a7915b685b63e09bbbd8e42025d71408
prakashranjan <prakashranjansingh04@gmail.com>
2018-02-14 09:25:34 +0530

    one signal push integration remove old push code

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -115,19 +115,5 @@ self.addEventListener('notificationclick', function(e) {
 });
 
 
-self.addEventListener('push', function(e) {
-  var options = {
-    body: 'This notification was generated from a push!',
-    icon: 'images/ic_launcher/res/mipmap-xhdpi/ic_launcher.png',
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: '2'
-    }
-   
-  };
-  e.waitUntil(
-    self.registration.showNotification('Hello world!', options)
-  );
-});
+
 
